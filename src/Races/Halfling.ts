ebe9433a9be9955c1101f938801a43d7a9f55788
Jaci-Xavier <devxvr@gmail.com>
2023-08-22 16:30:06 -0300

    req 02: fix: halfling life

--- src/Races/Halfling.ts
+++ src/Races/Halfling.ts
@@ -6,7 +6,7 @@ class Halfling extends Race {
 
   constructor(name: string, dexterity: number) {
     super(name, dexterity);
-    this._maxLifePoints = 99;
+    this._maxLifePoints = 60;
     Halfling.populationInstances += 1;
   }
 
