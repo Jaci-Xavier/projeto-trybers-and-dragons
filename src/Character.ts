399d62d236725e9834994f4ae82b3eb3fc1ab71c
Jaci-Xavier <devxvr@gmail.com>
2023-08-23 09:06:24 -0300

    req 09

--- src/Character.ts
+++ src/Character.ts
@@ -1,4 +1,4 @@
-import Fighter from './Fighter';
+import Fighter, { SimpleFighter } from './Fighter';
 import Race, { Elf } from './Races';
 import Archetype, { Mage } from './Archetypes';
 import Energy from './Energy';
@@ -75,11 +75,11 @@ class Character implements Fighter {
     return this._lifePoints;
   }
 
-  attack(enemy: Fighter): void {
+  attack(enemy: Fighter | SimpleFighter): void {
     enemy.receiveDamage(this.strength);
   }
 
-  special?(enemy: Fighter): void {
+  special?(enemy: Fighter | SimpleFighter): void {
     const bonusDamage = getRandomInt(1, 10);
     enemy.receiveDamage(this.strength * bonusDamage);
   }
